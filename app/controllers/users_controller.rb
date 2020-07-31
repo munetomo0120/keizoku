f2d006fc33e8726b93780b51d024a34d22af0afc
munetomo <fishmans0120@gmail.com>
2020-07-31 14:30:00 +0900

    add posts, users, comments, likes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,8 @@
+class UsersController < ApplicationController
+  def show
+    user = User.find(params[:id])
+    @user = User.find(params[:id])
+    @name = user.name
+    @posts = user.posts.order("created_at DESC")
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
   root "posts#index"
-  resources :posts
+  resources :posts do
+    resources :likes, only: [:create, :destroy]
+    resources :comments, only: [:create, :destroy]
+    collection do
+      get "search"
+    end
+  end
+  resources :users, only: [:show]
 end
